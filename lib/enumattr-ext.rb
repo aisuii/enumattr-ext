197cb072a62c03bb73d27eeb0cef9ee4b486dcbb
aisuii <aisuiiaisuii@gmail.com>
2012-05-24 00:15:22 +0900

    initial commit

---
+++ lib/enumattr-ext.rb
@@ -0,0 +1,7 @@
+require "enumattr-ext/version"
+
+module Enumattr
+  module Ext
+    # Your code goes here...
+  end
+end
---
+++ lib/enumattr-ext/version.rb
@@ -0,0 +1,5 @@
+module Enumattr
+  module Ext
+    VERSION = "0.0.1"
+  end
+end
